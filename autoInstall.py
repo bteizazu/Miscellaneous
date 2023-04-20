3d1c296e8a92b72426ab18b2957078f46c86786b
bteizazu <teiza002@umn.edu>
2023-04-20 08:06:29 -0400

    Create autoInstall.py

---
+++ autoInstall.py
@@ -0,0 +1,8 @@
+#INSTALL Homebrew
+## https://brew.sh
+### /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#ProtonVPN
+#VSCode
+#Jupyter
+#Accountable2U
